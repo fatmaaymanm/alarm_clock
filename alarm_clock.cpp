c773de48ff9a9f68b78952da83f15dc7a2020138
Nancy-Amr <144449880+Nancy-Amr@users.noreply.github.com>
2023-11-05 19:08:52 +0200

    Update alarm_clock.cpp

--- alarm_clock.cpp
+++ alarm_clock.cpp
@@ -3,15 +3,15 @@
 //
 
 #include "alarm_clock.h"
-LinkedList::LinkedList() : first(0), mySize(0) {}
+AlarmClock::AlarmClock() : first(0), mySize(0) {}
 
 // Definition of empty()
-bool LinkedList::empty() {
+bool AlarmClock::empty() {
     return mySize == 0;
 }
 //-- Definition of the destructor
-inline LinkedList::~LinkedList() {
-    LinkedList::NodePointer prev = first, ptr;
+inline AlarmClock::~AlarmClock() {
+    AlarmClock::NodePointer prev = first, ptr;
     while (prev != 0)
     {
         ptr = prev->next;
@@ -20,4 +20,44 @@ inline LinkedList::~LinkedList() {
     }
 }
 
+//-- Definition of erase()
+void AlarmClock::erase(int index) {
+    if (index < 0 || index >= mySize)
+    {
+        cerr << "Illegal location to delete -- " << index << endl;
+        return;
+    }
+    mySize--;
+    AlarmClock::NodePointer ptr,
+        predPtr = first;
+    if (index == 0)
+    {
+        ptr = first;
+        first = ptr->next;
+        delete ptr;
+    } else {
+        for (int i = 1; i < index; i++)
+            predPtr = predPtr->next;
+        ptr = predPtr->next;
+        predPtr->next = ptr->next;
+        delete ptr;
+    }
+}
+
+//-- Definition of the output operator
+ostream& operator<<(ostream& out, const AlarmClock& aList) {
+    aList.display(out);
+    return out;
+}
+
+istream& operator>>(istream& in, AlarmClock& aList) {
+    ElementType val;
+    in >> val;
+    aList.insert(val, aList.mySize); // Needed friend to access "mySize"
+    return in;
+}
+bool operator > (const AlarmClock& c1, const AlarmClock& c2)
+{
+    return (c1.hours>c2.hours && c1.minutes> c2.minutes && c1.seconds > c2.seconds && c1.isAm);
+}
 
