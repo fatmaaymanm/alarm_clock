ccd0e93e66285d7dfbc3b8848190f1f78c3f4fcd
Nancy-Amr <144449880+Nancy-Amr@users.noreply.github.com>
2023-11-05 18:36:43 +0200

    Update alarm_clock.cpp

--- alarm_clock.cpp
+++ alarm_clock.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "alarm_clock.h"
+LinkedList::LinkedList() : first(0), mySize(0) {}
+
+//-- Definition of the destructor
+inline LinkedList::~LinkedList() {
+    LinkedList::NodePointer prev = first, ptr;
+    while (prev != 0)
+    {
+        ptr = prev->next;
+        delete prev;
+        prev = ptr;
+    }
+}
